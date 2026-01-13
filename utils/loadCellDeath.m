a99637a60d1eba61a84bdd5312db07b7a6f12420
anttagjan <38495085+anttagjan@users.noreply.github.com>
2026-01-13 18:27:26 +0100

    some useful codes

--- utils/loadCellDeath.m
+++ utils/loadCellDeath.m
@@ -3,15 +3,15 @@ clear all
 %% load data
 filepath=('D:\Antonio\deXtrusion\tests');
 % filepath=('D:\Antonio\extrusion systematic characterisation\projections\');
-nf_division = dir(fullfile(filepath,'results','*cell_division.zip'));
-% nf_division = dir(fullfile(filepath,'results_cellDivision','*cell_division.zip'));
-filenames = {nf_division.name};
-noDivisions={};
+nf_death = dir(fullfile(filepath,'results','*cell_death.zip'));
+% nf_division = dir(fullfile(filepath,'results_cellDivision','*cell_death.zip'));
+filenames = {nf_death.name};
+noDeath={};
 %% Extraction of coordinates (landmarks, cell death events, masks)
 % Extraction division coordinates
 k = 0; coordinates = [];
-for i = 1:length(nf_division)
-    fname = fullfile(filepath,'results',nf_division(i).name);
+for i = 1:length(nf_death)
+    fname = fullfile(filepath,'results',nf_death(i).name);
    % fname = fullfile(filepath,'results_cellDivision',nf_division(i).name);
     ROI = ReadImageJROI(fname);
     L(i) = length(ROI);
@@ -21,8 +21,8 @@ for i = 1:length(nf_division)
         coordinates(k,3) = i;
         coordinates(k,4) = ROI{1,j}.vnPosition(3);
     end
-    noDivisions{i}=coordinates(coordinates(:,3)==i,4);
+    noDeath{i}=coordinates(coordinates(:,3)==i,4);
 end
 
-names=cellfun(@(x) split(x,'_cell_division.zip'),filenames,'UniformOutput',false);
+names=cellfun(@(x) split(x,'_cell_death.zip'),filenames,'UniformOutput',false);
 names=cellfun(@(x) x{1},names,'UniformOutput',false);
