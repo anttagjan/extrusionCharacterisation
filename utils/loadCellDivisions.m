6ce4c591d890d53822f05e386be40d67d2bad327
anttagjan <38495085+anttagjan@users.noreply.github.com>
2026-06-03 18:13:43 +0200

    Update loadCellDivisions.m

--- utils/loadCellDivisions.m
+++ utils/loadCellDivisions.m
@@ -1,18 +1,22 @@
 close all
 clear all
 %% load data
-filepath=('D:\Antonio\deXtrusion\caspase dynamics dataset');
-% filepath=('D:\Antonio\extrusion systematic characterisation\input_cellDivision');
-nf_division = dir(fullfile(filepath,'results','*cell_division.zip'));
-% nf_division = dir(fullfile(filepath,'results_cellDivision','*cell_division.zip'));
+% filepath=('D:\Antonio\deXtrusion\caspase dynamics dataset');
+filepath=('D:\Antonio\extrusion systematic characterisation\');
+% nf_division = dir(fullfile(filepath,'results','*cell_division.zip'));
+nf_division = dir(fullfile(filepath,'input_cellDivision','*cell_division.zip'));
+
 filenames = {nf_division.name};
 noDivisions={};
+
 %% Extraction of coordinates (landmarks, cell death events, masks)
 % Extraction division coordinates
 k = 0; coordinates = [];
 for i = 1:length(nf_division)
-    fname = fullfile(filepath,'results',nf_division(i).name);
-   % fname = fullfile(filepath,'results_cellDivision',nf_division(i).name);
+
+    % fname = fullfile(filepath,'results',nf_division(i).name);
+   fname = fullfile(filepath,'input_cellDivision',nf_division(i).name);
+
     ROI = ReadImageJROI(fname);
     L(i) = length(ROI);
     for j = 1:L(i)
