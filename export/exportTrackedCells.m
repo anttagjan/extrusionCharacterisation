c575da46471b6e81a1a59ae3eee2f92cb634b177
anttagjan <38495085+anttagjan@users.noreply.github.com>
2026-06-29 12:39:28 +0200

    Update exportTrackedCells.m

--- export/exportTrackedCells.m
+++ export/exportTrackedCells.m
@@ -6,13 +6,13 @@ fileData = dir(fullfile(filepath,'labeled','*.tif'));
 
 area_threshold = 2000;
 
-for nFile = 39:size(fileData,1)
+for nFile = 56:size(fileData,1)
     
     disp(fileData(nFile).name);
     
     %   Load files
     fileName=split(fileData(nFile).name,'_labeled.tif');
-    if ~exist(fullfile(filepath,'area',strcat(fileName{1},'_area.tif')),'file')
+    if ~exist(fullfile(filepath,'tracked',strcat(fileName{1},'_tracked.tif')),'file')
         [segmentedImage] = readStackTif(fullfile(fileData(nFile).folder,fileData(nFile).name));
         trackedCells = readtable(fullfile(filepath,'dataframes',strcat(fileName{1},'.csv')));
 
@@ -26,38 +26,38 @@ for nFile = 39:size(fileData,1)
         [trackedImage] = cellTrackingRelabeling(segmentedImage,trackedCells);
         writeStackTif(uint16(trackedImage),fullfile(filepath,'tracked',strcat(fileName{1},'_tracked.tif')));
 
-        if min(trackedCells.frame) < 1
-            trackedCells.frame=trackedCells.frame+1;
-        end
-
-        areaImage=zeros(size(trackedImage));
-        disp('Processing MORPHOLOGY features...');
-
-        for nFrame = 1:size(trackedImage,3)
-            actualImg = trackedImage(:,:,nFrame);
-            actualLabels = unique(actualImg);
-            actualLabels(actualLabels==0) = [];
-
-            actualAreaImage      = zeros(size(actualImg));
-            actualPerimeterImage = zeros(size(actualImg));
-
-            for nCell = 1:length(actualLabels)
-                label = actualLabels(nCell);
-                indx  = find(trackedCells.frame==nFrame & trackedCells.particle==label);
-                if ~isempty(indx)
-                    mask = actualImg == label;
-                    actualAreaImage(mask)      = trackedCells.area(indx);
-                    actualPerimeterImage(mask) = trackedCells.perimeter(indx);
-                end
-            end
-
-            areaImage(:,:,nFrame)      = actualAreaImage;
-
-            disp(['MORPHO → Frame ' num2str(nFrame)]);
-        end
+        % if min(trackedCells.frame) < 1
+        %     trackedCells.frame=trackedCells.frame+1;
+        % end
+        % 
+        % areaImage=zeros(size(trackedImage));
+        % disp('Processing MORPHOLOGY features...');
+        % 
+        % for nFrame = 1:size(trackedImage,3)
+        %     actualImg = trackedImage(:,:,nFrame);
+        %     actualLabels = unique(actualImg);
+        %     actualLabels(actualLabels==0) = [];
+        % 
+        %     actualAreaImage      = zeros(size(actualImg));
+        %     actualPerimeterImage = zeros(size(actualImg));
+        % 
+        %     for nCell = 1:length(actualLabels)
+        %         label = actualLabels(nCell);
+        %         indx  = find(trackedCells.frame==nFrame & trackedCells.particle==label);
+        %         if ~isempty(indx)
+        %             mask = actualImg == label;
+        %             actualAreaImage(mask)      = trackedCells.area(indx);
+        %             actualPerimeterImage(mask) = trackedCells.perimeter(indx);
+        %         end
+        %     end
+        % 
+        %     areaImage(:,:,nFrame)      = actualAreaImage;
+        % 
+        %     disp(['MORPHO → Frame ' num2str(nFrame)]);
+        % end
 
         % Export
-        writeStackTif(uint16(areaImage),fullfile(filepath,'area',strcat(fileName{1},'_area.tif')));
-        disp('Exported MORPHOLOGY and tracking');
+        % writeStackTif(uint16(areaImage),fullfile(filepath,'area',strcat(fileName{1},'_area.tif')));
+        % disp('Exported MORPHOLOGY and tracking');
     end
 end
